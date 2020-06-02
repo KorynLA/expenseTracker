19eccf35faa362d52e9244970c55a9a301c6a5ab
KorynLA <leslieak@oregonstate.edu>
2020-06-02 14:28:35 -0700

    Updating logic to use command line arguments to determine file names

--- src/expenseTracker/Main.java
+++ src/expenseTracker/Main.java
@@ -10,24 +10,29 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        //used to determine the file in the args array to be opened
+        int statementsToEnter = args.length-1;
 
-        String fileName ="venmo_statement.csv";
-        File statement = new File(fileName);
-        //Attempt to open file and go through every line
-        try {
-            Scanner readStatement = new Scanner(statement);
-            System.out.println("opening file");
-            while(readStatement.hasNextLine()) {
-                String parseStatement = readStatement.nextLine();
-                List<String> items = Arrays.asList(parseStatement.split(","));
-                System.out.println(items);
+        //Go through all files listed in command line to be entered into program
+        while(statementsToEnter > -1) {
+            String fileName = args[statementsToEnter];
+            File statement = new File(fileName);
+            //Attempt to open file and go through every line
+            try {
+                Scanner readStatement = new Scanner(statement);
+                while (readStatement.hasNextLine()) {
+                    String parseStatement = readStatement.nextLine();
+                    List<String> items = Arrays.asList(parseStatement.split(","));
+                    System.out.println(items);
+                }
+                readStatement.close();
             }
-            readStatement.close();
-        }
-        //If file is not found catch the error and print the error stack
-        catch(FileNotFoundException err) {
-            System.out.println("File not found!");
-            err.printStackTrace();
+            //If file is not found catch the error and print the error stack
+            catch (FileNotFoundException err) {
+                System.out.println("File not found!");
+                err.printStackTrace();
+            }
+            statementsToEnter--;
         }
     }
 }
